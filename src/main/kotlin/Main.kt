8d39da7516daf237331cc0d8cd81851863b54a41
Christopher Salvador Márquez Alvarez <a01225198@itesm.mx>
2019-03-29 15:33:35 -0600

    Final formatting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,15 @@
 lateinit var cryptex: RSA
 
 fun main() {
-/*    task1()
-    task2()
-    task3()
-    task4()
-    task5()*/
-    task6()
+    val tasks = arrayOf(task1, task2, task3, task4, task5, task6)
+    var taskNumber = 1
+    tasks.forEach {
+        println("\n ==============  Task ${taskNumber++} ================== \n")
+        it()
+    }
 }
 
-fun task1() {
+val task1 =  {
     val p = "F7E75FDC469067FFDC4E847C51F452DF".toBigInteger(16)
     val q = "E85CED54AF57E53E092113E62F436F4F".toBigInteger(16)
     val e = "0D88C3".toBigInteger(16)
@@ -18,7 +18,7 @@ fun task1() {
     println(cryptex.privateKey)
 }
 
-fun task2() {
+val task2 = {
     val n = "DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5".toBigInteger(16)
     val e = "010001".toBigInteger(16)
     val key = RSA.Key(e, n)
@@ -28,7 +28,7 @@ fun task2() {
 }
 
 
-fun task3() {
+val task3 = {
     val n = "DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5".toBigInteger(16)
     val d = "74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D".toBigInteger(16)
     val cipher = "8C0F971DF2F3672B28811407E2DABBE1DA0FEBBBDFC7DCB67396567EA1E2493F"
@@ -37,7 +37,7 @@ fun task3() {
     println("Decoded: $message")
 }
 
-fun task4() {
+val task4 = {
     val text = "I owe you \$2000."
     val alternateText = "I owe you \$3000."
     val textSignature = cryptex.sign(text)
@@ -46,7 +46,7 @@ fun task4() {
     println("Alternate: $alternateSignature")
 }
 
-fun task5() {
+val task5 = {
     val text = "Launch a missile."
     val signature = "643D6F34902D9C7EC90CB0B2BCA36C47FA37165C0005CAB026C0542CBDB6802F"
     val n = "AE1CD4DC432798D933779FBD46C6E1247F0CF1233595113AA51B450F18116115".toBigInteger(16)
@@ -60,7 +60,7 @@ fun task5() {
 }
 
 
-fun task6() {
+val task6 = {
     val modulus = RSA.loadFile("publicKey.txt").toBigInteger(16)
     val exponent = RSA.loadFile("exponent.txt").toBigInteger(16)
     val publicKey = RSA.Key(exponent, modulus)
